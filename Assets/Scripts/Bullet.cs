0ef66cf4bdba125ccc8113164daa7a8257d75a6a
jjmorales <jonjoemorales@gmail.com>
2020-05-03 03:03:41 -0400

    space ship prefab done

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+    public float speed = 20f;
+    public int attackDamage = 30;
+    public Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.velocity = transform.up * speed;
+    }
+    
+    void OnTriggerEnter2D(Collider2D col){
+        col.GetComponent<Enemy>().takeDamage(attackDamage);
+        Destroy(gameObject);
+
+    }
+}
---
+++ Assets/Scripts/SpaceShipMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceShipMovement : MonoBehaviour
+{
+
+    public CharacterController2D controller;
+    float horizontalMove = 0f;
+    public float flightSpeed = 40f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * flightSpeed;
+
+    }
+
+    void FixedUpdate(){
+        
+        //move character
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, false);
+    }
+}
