cfe8699e7ff781cf8853b06679b5bdf2b2ea3a8b
jjmorales <jonjoemorales@gmail.com>
2020-05-05 02:02:01 -0400

    UI for point system

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,12 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Enemy : MonoBehaviour
 {
 
     public int maxHealth = 100;
     public float speed = 2f;
+    public Text points;
     GameObject targetChase;
     int currHealth;
 
@@ -27,13 +29,25 @@ public class Enemy : MonoBehaviour
 
         // hurt animation
 
+        addPointsDmg();
         if(currHealth <= 0){
             die();
         }
     }
 
     void die(){
+        addPointsKill();
         Destroy(this.gameObject);
+    }
+
+    void addPointsKill(){
+        int totalPoints = int.Parse(points.text) + 100;
+        points.text = totalPoints.ToString();
+    }
+
+    void addPointsDmg(){
+        int totalPoints = int.Parse(points.text) + 25;
+        points.text = totalPoints.ToString();
     }
 
     void approachPlayer(){
