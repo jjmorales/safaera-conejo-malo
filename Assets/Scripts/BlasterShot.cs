3910bb4feea2d89433f96e05037baabd2d113739
jjmorales <jonjoemorales@gmail.com>
2020-05-14 19:52:25 -0400

    BlasterShot

--- Assets/Scripts/BlasterShot.cs
+++ Assets/Scripts/BlasterShot.cs
@@ -2,11 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpaceShipAttack : MonoBehaviour
+public class BlasterShot : MonoBehaviour
 {
     public Transform firePoint;
     public GameObject blaster;
-    PlayerAttack playerAttack;
 
     // Update is called once per frame
     void Update()
