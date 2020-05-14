e0770b827352bbc8d61d53b23dce7225b02c4548
jjmorales <jonjoemorales@gmail.com>
2020-05-14 19:43:03 -0400

    AI script documentation

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -5,38 +5,38 @@ using Pathfinding;
 
 public class EnemyAI : MonoBehaviour
 {
-    public Transform target;
-
-    public float speed = 200f;
-    public float nextWaypointDistance = 3f;
-
-    public Transform enemySprite;
+    public Transform target;    // chase this target
+    public float speed = 200f;  // movement speed of enemy
+    public float nextWaypointDistance = 3f; // final distance from target to enemy
+    public Transform enemySprite;   // enemy sprite asset
 
 
-    public Animator animator;
-    Path path;
-    int currentWaypoint;
-    bool endOfPath = false;
+    public Animator animator;   // animator for enemy
+    Path path;  // temp path of enemy to target
+    int currentWaypoint;    // current waypoint in current path
+    bool endOfPath = false; // check for out of bounds
 
-    Seeker seeker;
-    Rigidbody2D rb;
+    Seeker seeker;  // access from A* class
+    Rigidbody2D rb; // this enemy's rigid body for movemement
 
 
     // Start is called before the first frame update
     void Start()
     {
-        seeker = GetComponent<Seeker>();
-        rb = GetComponent<Rigidbody2D>();
+        seeker = GetComponent<Seeker>();    // set seeker
+        rb = GetComponent<Rigidbody2D>();   // set rigid body
 
-        InvokeRepeating("UpdatePath", 0f, 0.5f);
+        InvokeRepeating("UpdatePath", 0f, 0.5f);    // update path continuously
     }
 
+    // if seeker path is completed, generate a new one
     void UpdatePath(){
         if(seeker.IsDone()){
             seeker.StartPath(rb.position, target.position, OnPathComplete);
         }
     }
 
+    // generate new path
     void OnPathComplete(Path p){
         if(!p.error){
             path = p;
@@ -46,30 +46,25 @@ public class EnemyAI : MonoBehaviour
 
     void FixedUpdate()
     {
-
-
-        if(path == null){
-            animator.SetFloat("Speed", 0f);
+        if(path == null){   // check for invalid path or path finished
             return;
-        }else{
-            animator.SetFloat("Speed", 1f);
         }
 
-        if(currentWaypoint >= path.vectorPath.Count){
+        if(currentWaypoint >= path.vectorPath.Count){   // stop waypoints from generating out of bounds of path
             endOfPath = true;
             return;
         }else{
             endOfPath = false;
         }
 
-        Vector2 direction = ((Vector2)path.vectorPath[currentWaypoint] - rb.position).normalized;
-        Vector2 force = direction * speed * Time.deltaTime;
+        Vector2 direction = ((Vector2)path.vectorPath[currentWaypoint] - rb.position).normalized;   // directon of path
 
-        rb.AddForce(force);
+        Vector2 force = direction * speed * Time.deltaTime; // calculate speed for force to move enemy
+        rb.AddForce(force); // move enemy
 
-        float distance = Vector2.Distance(rb.position, path.vectorPath[currentWaypoint]);
+        float distance = Vector2.Distance(rb.position, path.vectorPath[currentWaypoint]);   // distance between enemy and next waypoint
 
-        if(distance < nextWaypointDistance){
+        if(distance < nextWaypointDistance){    // next point in path
             currentWaypoint++;
         }
 
