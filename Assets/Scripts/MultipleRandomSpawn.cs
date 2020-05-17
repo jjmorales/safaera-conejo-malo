3195513c005ed967d69add6511ef903880cd561f
jjmorales <jonjoemorales@gmail.com>
2020-05-17 00:54:54 -0400

    random spawn ships

---
+++ Assets/Scripts/MultipleRandomSpawn.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MultipleRandomSpawn : MonoBehaviour
+{
+
+    public Transform [] spawnPoints;
+    public GameObject spawnee;
+    public float cycleSpawn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("spawn", 0f, cycleSpawn);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void spawn(){
+        int temp = Random.Range(0,spawnPoints.Length);
+        Instantiate(spawnee, spawnPoints[temp].transform.position,  spawnPoints[temp].transform.rotation);
+
+    }
+}
---
+++ Assets/Scripts/ShipBehave.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipBehave : MonoBehaviour
+{
+
+    public string direction;
+    public int speed;
+    public int deathDelay;
+    public Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, deathDelay);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(direction == "Left" || direction == "Right"){
+            rb.velocity = transform.right * speed;
+        }else if(direction == "Down" || direction == "Up"){
+            rb.velocity = transform.up * speed;
+
+        }
+    }
+}
