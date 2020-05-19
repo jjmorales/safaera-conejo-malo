ecae8b6296e511c49a75d57cc947d56e2eec349a
jjmorales <jonjoemorales@gmail.com>
2020-05-19 06:08:13 -0400

    trex follow

---
+++ Assets/Scripts/Follow.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follow : MonoBehaviour
+{
+    public GameObject target;
+    public float speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, target.transform.position, Time.deltaTime * speed);
+    }
+}
