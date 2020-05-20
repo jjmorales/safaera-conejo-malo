652af828b8913fb75689c2b9df89fdc98bb4dd5f
jjmorales <jonjoemorales@gmail.com>
2020-05-20 05:56:53 -0400

    squid behave added

---
+++ Assets/Scripts/SquidBomb.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SquidBomb : MonoBehaviour
+{
+    public int AttackDamage;
+    public int speed;
+    public float chaseTime;
+    public Vector2 rangeRandomAttack;
+    public Animator animator;
+    public Collider2D col;
+    private float launchTime;
+    private bool launching = false;
+    private Transform lastPlayerPos;
+    private bool isDying = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        launchTime = Random.Range(rangeRandomAttack.x, rangeRandomAttack.y);
+        Invoke("attack", launchTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(launching){
+            transform.position = Vector2.MoveTowards(transform.position, lastPlayerPos.transform.position, Time.deltaTime * speed);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D col){
+        
+        if(col.gameObject.tag == "Player"){
+            launching = false;
+            animator.SetTrigger("Explode");
+            GameObject.FindGameObjectWithTag("Player").GetComponent<Player>().TakeDamage(AttackDamage);
+            Destroy (gameObject, this.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).length - 2); 
+
+        }
+    }
+
+    void attack(){
+        lastPlayerPos = GameObject.FindGameObjectWithTag("Player").transform;
+        launching = true;
+        Invoke("selfDestruct", chaseTime);
+    }
+
+    void selfDestruct(){
+        launching = false;
+        animator.SetTrigger("Explode");        
+        Destroy (gameObject, this.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).length - 2); 
+    }
+
+
+}
