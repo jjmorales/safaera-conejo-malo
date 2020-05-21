15b934394244a918e5658f53dd1e4340d2dd5e1c
jjmorales <jonjoemorales@gmail.com>
2020-05-20 23:47:06 -0400

    level select scene

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadLevel1(){
+        SceneManager.LoadScene(1);
+    }
+
+    public void LoadLevel2(){
+        SceneManager.LoadScene(2);
+    }
+    public void LoadLevel3(){
+        SceneManager.LoadScene(3);
+    }
+    public void LoadLevel4(){
+        SceneManager.LoadScene(4);
+    }
+    public void LoadLevel5(){
+        SceneManager.LoadScene(5);
+    }
+    public void LoadLevel6(){
+        SceneManager.LoadScene(6);
+    }
+    public void LoadStartMenu(){
+        SceneManager.LoadScene(0);
+    }
+
+    public void LoadLevelSelection(){
+        SceneManager.LoadScene(8);
+    }
+
+    public void LoadCredits(){
+        SceneManager.LoadScene(7);
+    }
+}
